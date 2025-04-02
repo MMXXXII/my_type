7c540f2e8515f019a7b3cd7f987c5a3ef42fa5a2
MMXXII <perfilev621@gmail.com>
2025-04-02 21:50:19 +0800

    СЛАВА С# 🙏🏼➕➕ СЛАВА СКРИПТАМ 🙏🏼➕➕ КОМПИЛЯТОРА ХРАНИТЕЛЯ КАЖДОМУ ИЗ ВАС 🙏🏼➕➕ БОЖЕ ХРАНИ С# 🙏🏼➕➕ СПАСИБО ВАМ НАШИ ПРОГРАММИСТЫ 🙏🏼➕➕ ЧТО ИСПОЛЬЗУЕТЕ С# 🙏🏼➕➕ ХРАНИ ВАС using System 🙏🏼➕➕
    p.s. 1) каждое введенное число теперь может быть в разной СС(двоичной, восьмиричной, десятичкой, шеснадцатиричной)
    2) Операции: сложение, вычитание, умножение, сравнение (одинаковые числа в разных СС указываются, как "Числа не равны" (это я тоже предусмотрел))
    3) Вывод так же может быть выведен в разных СС.

--- Вариант 6/Form1.Designer.cs
+++ Вариант 6/Form1.Designer.cs
@@ -29,66 +29,129 @@
         private void InitializeComponent()
         {
             txtFirst = new TextBox();
-            txtSecond = new TextBox();
             txtResult = new TextBox();
             cmbOperation = new ComboBox();
             comboBox1 = new ComboBox();
+            txtSecond = new TextBox();
+            comboBox2 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            comboBox3 = new ComboBox();
             SuspendLayout();
             // 
             // txtFirst
             // 
-            txtFirst.Location = new Point(90, 44);
+            txtFirst.Location = new Point(169, 41);
             txtFirst.Name = "txtFirst";
-            txtFirst.Size = new Size(125, 27);
+            txtFirst.Size = new Size(170, 27);
             txtFirst.TabIndex = 0;
             txtFirst.TextChanged += txtFirst_TextChanged;
             // 
-            // txtSecond
-            // 
-            txtSecond.Location = new Point(266, 44);
-            txtSecond.Name = "txtSecond";
-            txtSecond.Size = new Size(125, 27);
-            txtSecond.TabIndex = 1;
-            // 
             // txtResult
             // 
-            txtResult.Location = new Point(90, 252);
+            txtResult.Location = new Point(160, 213);
             txtResult.Name = "txtResult";
             txtResult.Size = new Size(327, 27);
             txtResult.TabIndex = 2;
             // 
             // cmbOperation
             // 
+            cmbOperation.Anchor = AnchorStyles.None;
             cmbOperation.FormattingEnabled = true;
-            cmbOperation.Items.AddRange(new object[] { "Сложение", "Вычитание", "Умножение", "Сравнение", "Вывод значения в любой СС" });
-            cmbOperation.Location = new Point(90, 99);
+            cmbOperation.Items.AddRange(new object[] { "Сложение", "Вычитание", "Умножение", "Сравнение" });
+            cmbOperation.Location = new Point(269, 165);
             cmbOperation.Name = "cmbOperation";
-            cmbOperation.Size = new Size(100, 28);
+            cmbOperation.Size = new Size(138, 28);
             cmbOperation.TabIndex = 3;
             cmbOperation.Text = "Сложение";
+            cmbOperation.SelectedIndexChanged += cmbOperation_SelectedIndexChanged;
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Двоичная", "Восьмиричная", "Десятичная", "Шеснадцатиричная" });
-            comboBox1.Location = new Point(90, 154);
+            comboBox1.Location = new Point(398, 41);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(151, 28);
             comboBox1.TabIndex = 4;
             comboBox1.Text = "Двоичная";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // txtSecond
+            // 
+            txtSecond.Location = new Point(169, 103);
+            txtSecond.Name = "txtSecond";
+            txtSecond.Size = new Size(170, 27);
+            txtSecond.TabIndex = 5;
+            txtSecond.TextChanged += txtSecond_TextChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Двоичная", "Восьмиричная", "Десятичная", "Шеснадцатиричная" });
+            comboBox2.Location = new Point(398, 103);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 6;
+            comboBox2.Text = "Двоичная";
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(45, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(114, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Первое число: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(48, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Второе число: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(48, 216);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Результат:";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "Двоичная", "Восьмиричная", "Десятичная", "Шеснадцатиричная" });
+            comboBox3.Location = new Point(516, 213);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(151, 28);
+            comboBox3.TabIndex = 10;
+            comboBox3.Text = "Двоичная";
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(688, 298);
+            Controls.Add(comboBox3);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(txtSecond);
             Controls.Add(comboBox1);
             Controls.Add(cmbOperation);
             Controls.Add(txtResult);
-            Controls.Add(txtSecond);
             Controls.Add(txtFirst);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Рассчеты числа с разными системами счисления";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -96,9 +159,14 @@
         #endregion
 
         private TextBox txtFirst;
-        private TextBox txtSecond;
         private TextBox txtResult;
         private ComboBox cmbOperation;
         private ComboBox comboBox1;
+        private TextBox txtSecond;
+        private ComboBox comboBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ComboBox comboBox3;
     }
 }
--- Вариант 6/Form1.cs
+++ Вариант 6/Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
 namespace Вариант_6
 {
     public partial class Form1 : Form
@@ -11,32 +15,127 @@ namespace Вариант_6
         {
             try
             {
-                var firstValue = double.Parse(txtFirst.Text);
-                var secondValue = double.Parse(txtSecond.Text);
+                // Читаем текст из полей, если пусто, то присваиваем "0"
+                string firstText = string.IsNullOrWhiteSpace(txtFirst.Text) ? "0" : txtFirst.Text;
+                string secondText = string.IsNullOrWhiteSpace(txtSecond.Text) ? "0" : txtSecond.Text;
 
-                var firstLength = new Length(firstValue, MeasureType.m);
-                var secondLength = new Length(secondValue, MeasureType.m);
+                // Выбор системы счисления для первого и второго числа
+                int baseFirst = GetBaseFromComboBox(comboBox1.SelectedItem.ToString());
+                int baseSecond = GetBaseFromComboBox(comboBox2.SelectedItem.ToString());
 
-                var sumLength = firstLength + secondLength;
+                // Проверка на правильность ввода в соответствии с выбранной системой счисления
+                if (!IsValidInput(firstText, baseFirst) || !IsValidInput(secondText, baseSecond))
+                {
+                    txtResult.Text = "Неверный ввод!";
+                    return;
+                }
 
-                txtResult.Text = sumLength.Verbose();
+                // Преобразуем из строки в десятичное число в зависимости от выбранной системы счисления
+                int firstValue = Convert.ToInt32(firstText, baseFirst);
+                int secondValue = Convert.ToInt32(secondText, baseSecond);
+
+                int resultValue = 0;
+
+                // Выполнение выбранной операции
+                switch (cmbOperation.Text)
+                {
+                    case "Сложение":
+                        resultValue = firstValue + secondValue;
+                        break;
+                    case "Вычитание":
+                        resultValue = firstValue - secondValue;
+                        break;
+                    case "Умножение":
+                        resultValue = firstValue * secondValue;
+                        break;
+                    case "Сравнение":
+                        txtResult.Text = firstValue == secondValue ? "Числа равны" : "Числа не равны";
+                        return;
+                    default:
+                        resultValue = 0;
+                        break;
+                }
+
+                // Выбор системы счисления для вывода результата
+                int resultBase = GetBaseFromComboBox(comboBox3.SelectedItem.ToString()); // Результат в системе, выбранной в comboBox3
+
+                string resultString = Convert.ToString(resultValue, resultBase).ToUpper();
+
+                // Выводим результат в поле txtResult
+                txtResult.Text = resultString;
             }
             catch (FormatException)
             {
-                // если тип преобразовать не смогли
+                txtResult.Text = "Ошибка ввода";
+            }
+            catch (OverflowException)
+            {
+                txtResult.Text = "Слишком большое число";
             }
         }
 
+        // Метод для получения базы из выбранной системы счисления
+        private int GetBaseFromComboBox(string selectedItem)
+        {
+            switch (selectedItem)
+            {
+                case "Двоичная":
+                    return 2;
+                case "Восьмиричная":
+                    return 8;
+                case "Десятичная":
+                    return 10;
+                case "Шестнадцатиричная":
+                    return 16;
+                default:
+                    return 10;
+            }
+        }
+
+        // Метод для проверки корректности ввода
+        private bool IsValidInput(string input, int numberBase)
+        {
+            string pattern = numberBase switch
+            {
+                2 => "^[01]*$",         // Для двоичной системы
+                8 => "^[0-7]*$",         // Для восьмиричной системы
+                10 => "^[0-9]*$",        // Для десятичной системы
+                16 => "^[0-9A-Fa-f]*$",  // Для шестнадцатиричной системы
+                _ => throw new ArgumentException("Неправильная система счисления")
+            };
+
+            return Regex.IsMatch(input, pattern);
+        }
+
+        // Обработчик изменения текста в первом поле
         private void txtFirst_TextChanged(object sender, EventArgs e)
         {
-            // старый код убрали, вставили вызов новой функции
-            Calculate();
+            Calculate(); // Вызываем перерасчет
         }
 
         private void txtSecond_TextChanged(object sender, EventArgs e)
         {
-            // старый код убрали, вставили вызов новой функции
+            Calculate(); // Вызываем перерасчет
+        }
+
+        private void cmbOperation_SelectedIndexChanged(object sender, EventArgs e)
+        {
             Calculate();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Calculate(); // Перерасчет при изменении первого комбо-бокса
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Calculate(); // Перерасчет при изменении второго комбо-бокса
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Calculate(); // Перерасчет при изменении третьего комбо-бокса (результат)
         }
     }
 }
