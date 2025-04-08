9cfec592a6e8e8fd35bbee6558a75305c3fbd912
MMXXII <perfilev621@gmail.com>
2025-04-08 21:38:57 +0800

    обновлены тесты

--- Вариант 6Tests/NumberBaseOperationsTests.cs
+++ Вариант 6Tests/NumberBaseOperationsTests.cs
@@ -9,25 +9,30 @@
             Assert.AreEqual(2, NumberBaseOperations.GetBase("Двоичная"));
             Assert.AreEqual(8, NumberBaseOperations.GetBase("Восьмиричная"));
             Assert.AreEqual(10, NumberBaseOperations.GetBase("Десятичная"));
-            Assert.AreEqual(16, NumberBaseOperations.GetBase("Шестнадцатиричная"));
-            Assert.AreEqual(10, NumberBaseOperations.GetBase("Что-то другое"));
+            Assert.AreEqual(16, NumberBaseOperations.GetBase("Шеснадцатиричная"));
+            Assert.AreEqual(10, NumberBaseOperations.GetBase("Нерпавильная система счисления"));
         }
 
         [TestMethod()]
         public void IsValidInputTest()
         {
-            Assert.IsTrue(NumberBaseOperations.IsValidInput("1010", 2));
-            Assert.IsFalse(NumberBaseOperations.IsValidInput("102", 2));
+            // Двоичная система
+            Assert.IsTrue(NumberBaseOperations.IsValidInput("1010", 2), "Двоичная: 1010 должно быть валидным");
+            Assert.IsFalse(NumberBaseOperations.IsValidInput("102", 2), "Двоичная: 102 не должно быть валидным");
 
-            Assert.IsTrue(NumberBaseOperations.IsValidInput("765", 8));
-            Assert.IsFalse(NumberBaseOperations.IsValidInput("89", 8));
+            // Восьмиричная система
+            Assert.IsTrue(NumberBaseOperations.IsValidInput("765", 8), "Восьмиричная: 765 должно быть валидным");
+            Assert.IsFalse(NumberBaseOperations.IsValidInput("899", 8), "Восьмиричная: 899 не должно быть валидным");
 
-            Assert.IsTrue(NumberBaseOperations.IsValidInput("1234567890", 10));
-            Assert.IsFalse(NumberBaseOperations.IsValidInput("12A", 10));
+            // Десятичная система
+            Assert.IsTrue(NumberBaseOperations.IsValidInput("1234567890", 10), "Десятичная: 1234567890 должно быть валидным");
+            Assert.IsFalse(NumberBaseOperations.IsValidInput("12A", 10), "Десятичная: 12A не должно быть валидным");
 
-            Assert.IsTrue(NumberBaseOperations.IsValidInput("1A3F", 16));
-            Assert.IsFalse(NumberBaseOperations.IsValidInput("XYZ", 16));
+            // Шестнадцатиричная система
+            Assert.IsTrue(NumberBaseOperations.IsValidInput("1A3F", 16), "Шестнадцатиричная: 1A3F должно быть валидным");
+            Assert.IsFalse(NumberBaseOperations.IsValidInput("XYZ", 16), "Шестнадцатиричная: XYZ не должно быть валидным");
         }
+
 
         [TestMethod()]
         public void ConvertToDecimalTest()
@@ -38,14 +43,7 @@
             Assert.AreEqual(255, NumberBaseOperations.ConvertToDecimal("FF", 16));
         }
 
-        [TestMethod()]
-        public void ConvertFromDecimalTest()
-        {
-            Assert.AreEqual("1010", NumberBaseOperations.ConvertFromDecimal(10, 2));
-            Assert.AreEqual("17", NumberBaseOperations.ConvertFromDecimal(15, 8));
-            Assert.AreEqual("255", NumberBaseOperations.ConvertFromDecimal(255, 10));
-            Assert.AreEqual("FF", NumberBaseOperations.ConvertFromDecimal(255, 16));
-        }
+
 
         [TestMethod()]
         public void PerformOperationTest()
@@ -55,7 +53,7 @@
             Assert.AreEqual("15", NumberBaseOperations.PerformOperation(5, 3, "Умножение"));
             Assert.AreEqual("Числа равны", NumberBaseOperations.PerformOperation(5, 5, "Сравнение"));
             Assert.AreEqual("Числа не равны", NumberBaseOperations.PerformOperation(5, 3, "Сравнение"));
-            Assert.AreEqual("Операция не выбрана", NumberBaseOperations.PerformOperation(5, 3, "Неизвестно"));
+            Assert.AreEqual("Операция не выбрана", NumberBaseOperations.PerformOperation(5, 3, " "));
         }
     }
 }
